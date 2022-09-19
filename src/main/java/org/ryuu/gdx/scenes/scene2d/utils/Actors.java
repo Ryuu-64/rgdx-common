004a03b845c68a54433f876803ec6a5d47e26668
WIN-KUYOU\DESKTOP <2357622935@qq.com>
2022-09-19 10:24:15 +0800

    remove Aligns util class, move its API to Actors util class

--- src/main/java/org/ryuu/gdx/scenes/scene2d/utils/Actors.java
+++ src/main/java/org/ryuu/gdx/scenes/scene2d/utils/Actors.java
@@ -7,6 +7,7 @@ public class Actors {
     private Actors() {
     }
 
+
     public static void align(Actor self, int alignSelf, Actor reference, int alignReference, float x, float y) {
         align(self, alignSelf, reference, alignReference);
         self.moveBy(x, y);
@@ -16,17 +17,17 @@ public class Actors {
         self.setPosition(reference.getX(alignReference), reference.getY(alignReference), alignSelf);
     }
 
-    public static void align(Actor self, int align, float x, float y) {
-        align(self, align);
-        self.moveBy(x, y);
+    public static void align(Actor actor, int alignSelf, int alignParent, float x, float y) {
+        align(actor, alignSelf, alignParent);
+        actor.moveBy(x, y);
     }
 
-    public static void align(Actor self, int align) {
-        Group parent = self.getParent();
-        self.setPosition(
-                parent.getX(align) - parent.getX(),
-                parent.getY(align) - parent.getY(),
-                align
+    public static void align(Actor actor, int alignSelf, int alignParent) {
+        Group parent = actor.getParent();
+        actor.setPosition(
+                parent.getX(alignParent) - parent.getX(),
+                parent.getY(alignParent) - parent.getY(),
+                alignSelf
         );
     }
 }
