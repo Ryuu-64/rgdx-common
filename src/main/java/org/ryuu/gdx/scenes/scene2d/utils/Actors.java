8dd22bf2a0b340c3ac2afa03cd271ef41a2725f0
WIN-KUYOU\DESKTOP <2357622935@qq.com>
2022-09-27 09:13:33 +0800

    add Actors align API

--- src/main/java/org/ryuu/gdx/scenes/scene2d/utils/Actors.java
+++ src/main/java/org/ryuu/gdx/scenes/scene2d/utils/Actors.java
@@ -7,13 +7,12 @@ public class Actors {
     private Actors() {
     }
 
-    public static void align(Actor self, int alignSelf, Actor reference, int alignReference) {
-        self.setPosition(reference.getX(alignReference), reference.getY(alignReference), alignSelf);
+    public static void align(Actor actor, int align) {
+        align(actor, align, align);
     }
 
-    public static void align(Actor self, int alignSelf, Actor reference, int alignReference, float x, float y) {
-        align(self, alignSelf, reference, alignReference);
-        self.moveBy(x, y);
+    public static void align(Actor actor, int align, float x, float y) {
+        align(actor, align, align, x, y);
     }
 
     public static void align(Actor actor, int alignSelf, int alignParent) {
@@ -30,12 +29,22 @@ public class Actors {
         actor.moveBy(x, y);
     }
 
-    public static void align(Actor actor, int align) {
-        align(actor, align, align);
+    public static void align(Actor self, Actor reference, int align) {
+        self.setPosition(reference.getX(align), reference.getY(align), align);
     }
 
-    public static void align(Actor actor, int align, float x, float y) {
-        align(actor, align, align, x, y);
+    public static void align(Actor self, Actor reference, int align, float x, float y) {
+        self.setPosition(reference.getX(align), reference.getY(align), align);
+        self.moveBy(x, y);
+    }
+
+    public static void align(Actor self, int alignSelf, Actor reference, int alignReference) {
+        self.setPosition(reference.getX(alignReference), reference.getY(alignReference), alignSelf);
+    }
+
+    public static void align(Actor self, int alignSelf, Actor reference, int alignReference, float x, float y) {
+        align(self, alignSelf, reference, alignReference);
+        self.moveBy(x, y);
     }
 
     public static void setSize(Actor self, Actor reference) {
