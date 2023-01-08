4e3eb2bd68c6e9e56c6d3145a9eb7ef0230321a8
DESKTOP-FQ830JM\Ryuu <2357622935@qq.com>
2023-01-08 15:50:03 +0800

    SoundManager replace Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate() with java.util.concurrent.Executor
    SoundManager.SoundWrapper

--- src/main/java/org/ryuu/gdx/audio/SoundManager.java
+++ src/main/java/org/ryuu/gdx/audio/SoundManager.java
@@ -9,6 +9,7 @@ import org.ryuu.functional.IFunc1Arg;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 
@@ -17,165 +18,153 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 public class SoundManager {
     @Getter
     private final static Logger logger = Logger.getLogger(SoundManager.class.getName());
-    private final List<IAction> playSoundList = new ArrayList<>();
-    private final List<IAction> executePlaySoundList = new ArrayList<>();
-    @Getter
-    @Setter
-    private long interval;
-    @Getter
-    @Setter
-    private IFunc1Arg<String, Sound> getSoundByPath;
+    private final Executor executor;
+    private final IFunc1Arg<String, Sound> getSoundByPath;
 
-    public SoundManager(long interval, IFunc1Arg<String, Sound> getSoundByPath) {
-        this.interval = interval;
+    public SoundManager(Executor executor, IFunc1Arg<String, Sound> getSoundByPath) {
+        this.executor = executor;
         this.getSoundByPath = getSoundByPath;
-
-        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(() -> {
-            synchronized (playSoundList) {
-                if (!playSoundList.isEmpty()) {
-                    executePlaySoundList.addAll(playSoundList);
-                    playSoundList.clear();
-                }
-            }
-            while (!executePlaySoundList.isEmpty()) {
-                executePlaySoundList.remove(0).invoke();
-            }
-        }, 0, this.interval, MILLISECONDS);
     }
 
     public SoundWrapper play(String path) {
-        synchronized (playSoundList) {
-            SoundWrapper soundWrapper = new SoundWrapper();
-            playSoundList.add(() -> {
-                Sound sound = getSoundByPath.invoke(path);
-                soundWrapper.sound = sound;
-                soundWrapper.soundId = sound.play();
-                logger.info(
-                        "play {" +
-                                "soundManager=" + this +
-                                ", sound=" + sound +
-                                ", soundId=" + soundWrapper.soundId +
-                                ", path=" + path +
-                                "}"
-                );
-            });
-            return soundWrapper;
-        }
+        SoundWrapper soundWrapper = new SoundWrapper();
+        executor.execute(() -> {
+            Sound sound = getSoundByPath.invoke(path);
+            long soundId = sound.play();
+
+            soundWrapper.sound = sound;
+            soundWrapper.soundId = soundId;
+
+            logger.info(
+                    "play {" +
+                            "soundManager=" + this +
+                            ", sound=" + sound +
+                            ", soundId=" + soundId +
+                            ", path=" + path +
+                            "}"
+            );
+        });
+        return soundWrapper;
     }
 
     public SoundWrapper play(String path, float volume) {
-        synchronized (playSoundList) {
-            SoundWrapper soundWrapper = new SoundWrapper();
-            playSoundList.add(() -> {
-                Sound sound = getSoundByPath.invoke(path);
-                soundWrapper.sound = sound;
-                soundWrapper.soundId = sound.play(volume);
-                logger.info(
-                        "play {" +
-                                "soundManager=" + this +
-                                ", sound=" + sound +
-                                ", soundId=" + soundWrapper.soundId +
-                                ", path=" + path +
-                                ", volume=" + volume +
-                                "}"
-                );
-            });
-            return soundWrapper;
-        }
+        SoundWrapper soundWrapper = new SoundWrapper();
+        executor.execute(() -> {
+            Sound sound = getSoundByPath.invoke(path);
+            long soundId = sound.play(volume);
+
+            soundWrapper.sound = sound;
+            soundWrapper.soundId = soundId;
+
+            logger.info(
+                    "play {" +
+                            "soundManager=" + this +
+                            ", sound=" + sound +
+                            ", soundId=" + soundId +
+                            ", path=" + path +
+                            ", volume=" + volume +
+                            "}"
+            );
+        });
+        return soundWrapper;
     }
 
     public SoundWrapper play(String path, float volume, float pitch, float pan) {
-        synchronized (playSoundList) {
-            SoundWrapper soundWrapper = new SoundWrapper();
-            playSoundList.add(() -> {
-                Sound sound = getSoundByPath.invoke(path);
-                soundWrapper.sound = sound;
-                soundWrapper.soundId = sound.play(volume, pitch, pan);
-                logger.info(
-                        "play {" +
-                                "soundManager=" + this +
-                                ", sound=" + sound +
-                                ", soundId=" + soundWrapper.soundId +
-                                ", path=" + path +
-                                ", volume=" + volume +
-                                ", pitch=" + pitch +
-                                ", pan=" + pan +
-                                "}"
-                );
-            });
-            return soundWrapper;
-        }
+        SoundWrapper soundWrapper = new SoundWrapper();
+        executor.execute(() -> {
+            Sound sound = getSoundByPath.invoke(path);
+            long soundId = sound.play(volume, pitch, pan);
+
+            soundWrapper.sound = sound;
+            soundWrapper.soundId = soundId;
+
+            logger.info(
+                    "play {" +
+                            "soundManager=" + this +
+                            ", sound=" + sound +
+                            ", soundId=" + soundId +
+                            ", path=" + path +
+                            ", volume=" + volume +
+                            ", pitch=" + pitch +
+                            ", pan=" + pan +
+                            "}"
+            );
+        });
+        return soundWrapper;
     }
 
     public SoundWrapper loop(String path) {
-        synchronized (playSoundList) {
-            SoundWrapper soundWrapper = new SoundWrapper();
-            playSoundList.add(() -> {
-                Sound sound = getSoundByPath.invoke(path);
-                soundWrapper.sound = sound;
-                soundWrapper.soundId = sound.loop();
-                logger.info(
-                        "loop {" +
-                                "soundManager=" + this +
-                                ", sound=" + sound +
-                                ", soundId=" + soundWrapper.soundId +
-                                ", path=" + path +
-                                "}"
-                );
-            });
-            return soundWrapper;
-        }
+        SoundWrapper soundWrapper = new SoundWrapper();
+        executor.execute(() -> {
+            Sound sound = getSoundByPath.invoke(path);
+            long soundId = sound.loop();
+
+            soundWrapper.sound = sound;
+            soundWrapper.soundId = soundId;
+
+            logger.info(
+                    "loop {" +
+                            "soundManager=" + this +
+                            ", sound=" + sound +
+                            ", soundId=" + soundId +
+                            ", path=" + path +
+                            "}"
+            );
+        });
+        return soundWrapper;
     }
 
     public SoundWrapper loop(String path, float volume) {
-        synchronized (playSoundList) {
-            SoundWrapper soundWrapper = new SoundWrapper();
-            playSoundList.add(() -> {
-                Sound sound = getSoundByPath.invoke(path);
-                soundWrapper.sound = sound;
-                soundWrapper.soundId = sound.loop(volume);
-                logger.info(
-                        "loop {" +
-                                "soundManager=" + this +
-                                ", sound=" + sound +
-                                ", soundId=" + soundWrapper.soundId +
-                                ", path=" + path +
-                                ", volume=" + volume +
-                                "}"
-                );
-            });
-            return soundWrapper;
-        }
+        SoundWrapper soundWrapper = new SoundWrapper();
+        executor.execute(() -> {
+            Sound sound = getSoundByPath.invoke(path);
+            long soundId = sound.loop(volume);
+
+            soundWrapper.sound = sound;
+            soundWrapper.soundId = soundId;
+
+            logger.info(
+                    "loop {" +
+                            "soundManager=" + this +
+                            ", sound=" + sound +
+                            ", soundId=" + soundId +
+                            ", path=" + path +
+                            ", volume=" + volume +
+                            "}"
+            );
+        });
+        return soundWrapper;
     }
 
     public SoundWrapper loop(String path, float volume, float pitch, float pan) {
-        synchronized (playSoundList) {
-            SoundWrapper soundWrapper = new SoundWrapper();
-            playSoundList.add(() -> {
-                Sound sound = getSoundByPath.invoke(path);
-                soundWrapper.sound = sound;
-                soundWrapper.soundId = sound.loop(volume, pitch, pan);
-                logger.info(
-                        "loop {" +
-                                "soundManager=" + this +
-                                ", sound=" + sound +
-                                ", soundId=" + soundWrapper.soundId +
-                                ", path=" + path +
-                                ", volume=" + volume +
-                                ", pitch=" + pitch +
-                                ", pan=" + pan +
-                                "}"
-                );
-            });
-            return soundWrapper;
-        }
+        SoundWrapper soundWrapper = new SoundWrapper();
+        executor.execute(() -> {
+            Sound sound = getSoundByPath.invoke(path);
+            long soundId = sound.loop(volume, pitch, pan);
+
+            soundWrapper.sound = sound;
+            soundWrapper.soundId = soundId;
+
+            logger.info(
+                    "loop {" +
+                            "soundManager=" + this +
+                            ", sound=" + sound +
+                            ", soundId=" + soundId +
+                            ", path=" + path +
+                            ", volume=" + volume +
+                            ", pitch=" + pitch +
+                            ", pan=" + pan +
+                            "}"
+            );
+        });
+        return soundWrapper;
     }
 
     @ToString
     public static class SoundWrapper {
         @Getter
-        private long soundId = -1;
+        private volatile long soundId = -1;
         @Getter
-        private Sound sound;
+        private volatile Sound sound = null;
     }
 }
