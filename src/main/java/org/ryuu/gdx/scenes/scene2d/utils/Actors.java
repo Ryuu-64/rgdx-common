bbcc5a596b4911f4269e3a43670621670c584155
DESKTOP-FQ830JM\Ryuu <2357622935@qq.com>
2023-03-09 23:49:22 +0800

    refactor actors

--- src/main/java/org/ryuu/gdx/scenes/scene2d/utils/Actors.java
+++ src/main/java/org/ryuu/gdx/scenes/scene2d/utils/Actors.java
@@ -1,11 +1,11 @@
 package org.ryuu.gdx.scenes.scene2d.utils;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.Align;
 
 import static com.badlogic.gdx.utils.Align.*;
 
@@ -18,32 +18,36 @@ public class Actors {
             return;
         }
 
+        Group actorParent = actor.getParent();
+        Group referenceParent = reference.getParent();
+        if (reference == actorParent || actorParent == referenceParent) {
+            actor.setPosition(reference.getX(alignReference), reference.getY(alignReference), alignSelf);
+            return;
+        }
+
         Stage actorStage = actor.getStage();
         Group actorRoot = actorStage.getRoot();
         if (reference == actorRoot) {
             actor.setPosition(
-                    actorStage.getWidth() * pivotX(alignReference),
-                    actorStage.getHeight() * pivotY(alignReference),
+                    actorStage.getWidth() * alignX(alignReference),
+                    actorStage.getHeight() * alignY(alignReference),
                     alignSelf
             );
             return;
         }
 
         Stage referenceStage = reference.getStage();
-
         if (actorStage != referenceStage) {
             Vector3 vector3 = new Vector3(reference.getX(alignReference), reference.getY(alignReference), 0);
             referenceStage.getCamera().project(vector3);
+            vector3.y = Gdx.graphics.getHeight() - vector3.y;
             actorStage.getCamera().unproject(vector3);
             actor.setPosition(vector3.x, vector3.y, alignSelf);
             return;
         }
 
-        actor.setPosition(
-                reference.getX(alignReference) - reference.getX(),
-                reference.getY(alignReference) - reference.getY(),
-                alignSelf
-        );
+        Vector2 localToActorPosition = getLocalToActorPosition(reference, alignReference, actor.getParent());
+        actor.setPosition(localToActorPosition.x, localToActorPosition.y, alignSelf);
     }
 
     public static void align(Actor actor, int alignSelf, Actor reference, int alignReference, float x, float y) {
@@ -103,41 +107,35 @@ public class Actors {
     }
 
     public static Vector2 getLocalToStagePosition(Actor actor, int pivot) {
-        return getLocalToStagePosition(actor, pivot, 0, 0);
+        return actor.localToStageCoordinates(new Vector2(actor.getWidth() * alignX(pivot), actor.getHeight() * alignY(pivot)));
     }
 
     public static Vector2 getLocalToStagePosition(Actor actor, int pivot, float x, float y) {
-        return actor.localToStageCoordinates(new Vector2(actor.getWidth() * pivotX(pivot), actor.getHeight() * pivotY(pivot))).add(x, y);
+        return getLocalToStagePosition(actor, pivot).add(x, y);
     }
 
     public static Vector2 getLocalToActorPosition(Actor actor, int pivot, Actor reference) {
-        return getLocalToActorPosition(actor, pivot, reference, 0, 0);
+        return actor.localToActorCoordinates(reference, new Vector2(actor.getWidth() * alignX(pivot), actor.getHeight() * alignY(pivot)));
     }
 
     public static Vector2 getLocalToActorPosition(Actor actor, int pivot, Actor reference, float x, float y) {
-        return actor.localToActorCoordinates(reference, new Vector2(actor.getWidth() * pivotX(pivot), actor.getHeight() * pivotY(pivot))).add(x, y);
+        return getLocalToActorPosition(actor, pivot, reference).add(x, y);
     }
 
-    /**
-     * @param pivot {@link Align}
-     */
-    private static float pivotX(int pivot) {
-        if ((pivot & left) != 0) {
+    private static float alignX(int align) {
+        if ((align & left) != 0) {
             return 0;
-        } else if ((pivot & right) != 0) {
+        } else if ((align & right) != 0) {
             return 1;
         } else {
             return .5f;
         }
     }
 
-    /**
-     * @param pivot {@link Align}
-     */
-    private static float pivotY(int pivot) {
-        if ((pivot & bottom) != 0) {
+    private static float alignY(int align) {
+        if ((align & bottom) != 0) {
             return 0;
-        } else if ((pivot & top) != 0) {
+        } else if ((align & top) != 0) {
             return 1;
         } else {
             return .5f;
