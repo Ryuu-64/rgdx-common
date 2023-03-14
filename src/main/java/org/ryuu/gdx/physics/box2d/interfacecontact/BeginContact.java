f85795b002e5821e8bf3266c633840252d255767
WIN-KUYOU\DESKTOP <2357622935@qq.com>
2023-03-14 09:22:49 +0800

    Move some implementations to other repositories. Please refer to the link below: https://github.com/Ryuu-64/rgdx-core https://github.com/Ryuu-64/rgdx-box2d https://github.com/Ryuu-64/rgdx-audio https://github.com/Ryuu-64/gdx-box2d-interface-contact-listener

--- src/main/java/org/ryuu/gdx/physics/box2d/interfacecontact/BeginContact.java
+++
@@ -1,9 +0,0 @@
-package org.ryuu.gdx.physics.box2d.interfacecontact;
-
-import com.badlogic.gdx.physics.box2d.Contact;
-import com.badlogic.gdx.physics.box2d.Fixture;
-
-@FunctionalInterface
-public interface BeginContact {
-    void beginContact(Contact contact, Fixture self, Fixture other);
-}
--- src/main/java/org/ryuu/gdx/physics/box2d/interfacecontact/EndContact.java
+++
@@ -1,9 +0,0 @@
-package org.ryuu.gdx.physics.box2d.interfacecontact;
-
-import com.badlogic.gdx.physics.box2d.Contact;
-import com.badlogic.gdx.physics.box2d.Fixture;
-
-@FunctionalInterface
-public interface EndContact {
-    void endContact(Contact contact, Fixture self, Fixture other);
-}
