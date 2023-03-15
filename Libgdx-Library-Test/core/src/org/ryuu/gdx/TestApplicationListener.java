690c691e3e6076bbc8617059185807a555280634
DESKTOP-FQ830JM\Ryuu <2357622935@qq.com>
2023-03-15 23:17:59 +0800

    fix code style Document empty method body The utility class name '$assets' doesn't match '[A-Z][a-zA-Z0-9]*'

--- Libgdx-Library-Test/core/src/org/ryuu/gdx/TestApplicationListener.java
+++ Libgdx-Library-Test/core/src/org/ryuu/gdx/TestApplicationListener.java
@@ -1,29 +1,6 @@
 package org.ryuu.gdx;
 
-import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.ApplicationAdapter;
 
-public class TestApplicationListener implements ApplicationListener {
-    @Override
-    public void create() {
-    }
-
-    @Override
-    public void resize(int width, int height) {
-    }
-
-    @Override
-    public void render() {
-    }
-
-    @Override
-    public void pause() {
-    }
-
-    @Override
-    public void resume() {
-    }
-
-    @Override
-    public void dispose() {
-    }
+public class TestApplicationListener extends ApplicationAdapter {
 }
