75c5e82afb542e4593198121ac23c7e114fa416f
WIN-KUYOU\DESKTOP <2357622935@qq.com>
2023-03-15 15:09:46 +0800

    remove style click listener

--- src/main/java/org/ryuu/gdx/scenes/scene2d/utils/ClickListenerFactory.java
+++ src/main/java/org/ryuu/gdx/scenes/scene2d/utils/ClickListenerFactory.java
@@ -1,71 +1,18 @@
 package org.ryuu.gdx.scenes.scene2d.utils;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import org.ryuu.functional.Action;
-import org.ryuu.gdx.graphics.glutils.GetMaterial;
-import org.ryuu.gdx.graphics.glutils.Material;
-import org.ryuu.gdx.graphics.glutils.MaterialProperty;
-import org.ryuu.gdx.graphics.glutils.SetMaterial;
 import org.ryuu.gdx.scenes.scene2d.AdvanceClickListener;
-
-import static com.badlogic.gdx.scenes.scene2d.actions.Actions.scaleTo;
-import static org.ryuu.gdx.graphics.glutils.utils.Shaders.*;
 
 public class ClickListenerFactory {
     private ClickListenerFactory() {
-    }
-
-    public static AdvanceClickListener clickListener(Actor actor, float clickInterval, float delay, Action onClick) {
-        return new AdvanceClickListener(clickInterval, (event, x, y) -> actor.addAction(ActionUtil.delay(delay, onClick)));
     }
 
     public static AdvanceClickListener clickListener(Action onClick) {
         return new AdvanceClickListener(0, (event, x, y) -> onClick.invoke());
     }
 
-    public static AdvanceClickListener colorChange(Actor actor, Color color, float intensity) {
-        return new AdvanceClickListener(0) {
-            private Material actorMaterial;
-
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                if (actor instanceof MaterialProperty) {
-                    actorMaterial = ((GetMaterial) actor).getMaterial();
-                    Material material = new Material();
-                    material.setShaderProgram(HDR);
-                    material.setAttributef(HDR_COLOR_ATTRIBUTE, color.r, color.g, color.b, color.a);
-                    material.setAttributef(INTENSITY_ATTRIBUTE, intensity, 0, 0, 0);
-                    ((SetMaterial) actor).setMaterial(material);
-                }
-                return true;
-            }
-
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                if (actor instanceof MaterialProperty) {
-                    ((SetMaterial) actor).setMaterial(actorMaterial);
-                }
-            }
-        };
-    }
-
-    public static AdvanceClickListener sizeChange(Actor actor, float downScale, float downScaleDuration, float upScale, float upScaleDuration) {
-        return new AdvanceClickListener(0) {
-            private final float touchDownScaleX = actor.getScaleX();
-            private final float touchDownScaleY = actor.getScaleY();
-
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                actor.addAction(scaleTo(downScale * touchDownScaleX, downScale * touchDownScaleY, downScaleDuration));
-                return true;
-            }
-
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                actor.addAction(scaleTo(upScale * touchDownScaleX, upScale * touchDownScaleY, upScaleDuration));
-            }
-        };
+    public static AdvanceClickListener clickListener(Actor actor, float clickInterval, float delay, Action onClick) {
+        return new AdvanceClickListener(clickInterval, (event, x, y) -> actor.addAction(ActionUtil.delay(delay, onClick)));
     }
 }
--- src/main/java/org/ryuu/gdx/scenes/scene2d/utils/ClickListeners.java
+++ src/main/java/org/ryuu/gdx/scenes/scene2d/utils/ClickListeners.java
@@ -1,10 +1,9 @@
 package org.ryuu.gdx.scenes.scene2d.utils;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import org.ryuu.functional.Action;
 
-import static org.ryuu.gdx.scenes.scene2d.utils.ClickListenerFactory.*;
+import static org.ryuu.gdx.scenes.scene2d.utils.ClickListenerFactory.clickListener;
 
 public class ClickListeners {
     private ClickListeners() {
@@ -17,26 +16,6 @@ public class ClickListeners {
 
     public static <T extends Actor> T addClickListener(T actor, float clickInterval, float delay, Action onClick) {
         actor.addListener(clickListener(actor, clickInterval, delay, onClick));
-        return actor;
-    }
-
-    public static <T extends Actor> T addColorChange(T actor, Color color) {
-        actor.addListener(colorChange(actor, color, 1));
-        return actor;
-    }
-
-    public static <T extends Actor> T addColorChange(T actor, Color color, float intensity) {
-        actor.addListener(colorChange(actor, color, intensity));
-        return actor;
-    }
-
-    public static <T extends Actor> T addSizeChange(T actor) {
-        actor.addListener(sizeChange(actor, 1.05f, 1 / 60f * 5, 1, 1 / 60f * 5));
-        return actor;
-    }
-
-    public static <T extends Actor> T addSizeChange(T actor, float downScale, float downScaleDuration, float upScale, float upScaleDuration) {
-        actor.addListener(sizeChange(actor, downScale, downScaleDuration, upScale, upScaleDuration));
         return actor;
     }
 }
